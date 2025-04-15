aec999d983cfa0ac77b62597194028749ea122c1
PanisaBD <panisa2548@gmail.com>
2025-04-16 00:25:31 +0700

    SE review function

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {getCampgrounds, getCampground, createCampground, updateCampground, deleteCampground} = require('../controllers/campgrounds');
 //Include other resource routers
 const bookingRouter=require('./bookings');
+const reviews = require('./reviews');
 
 const router = express.Router();
 
@@ -9,6 +10,8 @@ const {protect, authorize} = require('../middleware/auth');
 
 //Re-route into other resource routers
 router.use('/:campgroundId/bookings/',bookingRouter);
+//for reviews
+router.use('/:campgroundId/reviews', reviews); 
 
 
 router.route('/').get(getCampgrounds).post(protect, authorize('admin'), createCampground);
